fcd2988bb910fcdc32491e98e8479aa154f12e72
laidai <laidai9966@gmail.com>
2025-02-12 13:09:25 +0700

    rename env

--- src/env.js
+++ src/env.js
@@ -25,7 +25,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    NEXT_PUBLIC_WEBSITE_URL: z.string().url(),
+    NEXT_PUBLIC_BASE_URL: z.string().url(),
     NEXT_PUBLIC_API_ENDPOINT_URL: z.string().url(),
   },
 
@@ -38,7 +38,7 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     SUFFIX_DEFAULT_ACCESS_KEY: process.env.SUFFIX_DEFAULT_ACCESS_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
-    NEXT_PUBLIC_WEBSITE_URL: process.env.NEXT_PUBLIC_WEBSITE_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     NEXT_PUBLIC_API_ENDPOINT_URL: process.env.NEXT_PUBLIC_API_ENDPOINT_URL,
   },
   /**
